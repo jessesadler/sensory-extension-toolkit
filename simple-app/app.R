85d9773bba0a8600b2942ae8cc52b7a774078eed
Jesse Sadler <jrsadler@icloud.com>
2026-03-17 20:23:42 -0400

    Add run button

--- simple-app/app.R
+++ simple-app/app.R
@@ -32,17 +32,40 @@ ui <- fluidPage(
                       selectInput("prior", "Your expectation:",
                                   choices = prior_tbl$certainty,
                                   width = "100%"
-                      )
+                      ),
+                      actionButton("simulate", "Simulate!")
                     ),
                     
                     # Show a plot of the generated distribution
                     mainPanel(
-                      textOutput("ab"),
-                      plotOutput("bayesPlot"),
                       textOutput("reportText"),
                       plotOutput("reportPlot")
                     )
                   )
+      ),
+      tabPanel("Distribution",
+               # Sidebar with a slider input for number of bins 
+               sidebarLayout(
+                 sidebarPanel(
+                   numericInput("N", 
+                                label = "Number of participants",
+                                value = 0, min = 0),
+                   numericInput("X",
+                                label = "Number of participants who thought they were different",
+                                value = 0, min = 0),
+                   selectInput("prior", "Your expectation:",
+                               choices = prior_tbl$certainty,
+                               width = "100%"
+                   ),
+                   actionButton("simulate", "Simulate!")
+                 ),
+                 
+                 # Show a plot of the generated distribution
+                 mainPanel(
+                   textOutput("ab"),
+                   plotOutput("bayesPlot")
+                 )
+               )
       )
     )
 )
@@ -55,17 +78,23 @@ server <- function(input, output) {
   output$ab <- renderText(
     paste("Alpha is currently:", priors()[[1]], "and",
           "Beta is currently:", priors()[[2]]))
+  
+  
 
   output$bayesPlot <- renderPlot({
     bayesrules::plot_beta_binomial(alpha = priors()[[1]], beta = priors()[[2]], y = input$X, n = input$N)
   })
   
+  N <- eventReactive(input$simulate, input$N)
+  X <- eventReactive(input$simulate, input$X)
+  
+  
   output$reportText <- renderText(
-    report_txt(input$N, input$X)
+    report_txt(N(), X())
   )
   
   output$reportPlot <- renderPlot({
-    report_plt(input$N, input$X)
+    report_plt(N(), X())
   })
 }
 
